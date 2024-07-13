ef0ce8cec3a937420dc712de04347cc221b16281
subbuj1210 <137508963+subbuj1210@users.noreply.github.com>
2024-07-13 07:45:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,11 +13,5 @@ module "app1-rg" {
         source = "git@github.com:subbuj1210/Practice-Devops.git"
         rg_name = var.rg_name
         rg_location = var.rg_location
-# Terraform State Storage to Azure Storage Container
-  backend "azurerm" {
-    resource_group_name   = "shoppingclient"
-    storage_account_name  = "shoppingclient553"
-    container_name        = "akspractice"
-    key                   = "terraform.tfstate"
-  }
+
 }
