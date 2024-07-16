564d267c202bcd982c7ea06a622612f74c0394ae
subbuj1210 <137508963+subbuj1210@users.noreply.github.com>
2024-07-16 12:21:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,3 +22,13 @@ module "app1-rg" {
         rg_name = "app1-rg3"
         rg_location = "westus"
 }
+module "app1-vnet" {
+        source = "git@github.com:subbuj1210/Practice-Devops.git"
+        rg_name = "app1-rg3"
+        rg_location = "app1-rg3"
+        vnet_name = "app1-vnet"
+        vnet_cidr = ["192.168.0.0/16"]
+        web_subnet_name = "websubnet"
+        web_subnet_address = ["192.168.0.0/24"]
+}
+
