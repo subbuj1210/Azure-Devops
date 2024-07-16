3ecc079f9d56bb4b82f6086d9e672576c264367b
subbuj1210 <137508963+subbuj1210@users.noreply.github.com>
2024-07-16 14:10:34 +0530

    Add files via upload

---
+++ modules/NSG/Varibale.tf
@@ -0,0 +1,12 @@
+# Azure Resource Group Name
+variable "rg_name" {
+  type = string
+}
+# Azure Resources Location
+variable "rg_location" {
+  type = string
+}
+
+variable "nsg_name" {
+  type = string
+}
