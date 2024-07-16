b90d3add4f4554b0db9203a494c14ac8f28261df
subbuj1210 <137508963+subbuj1210@users.noreply.github.com>
2024-07-16 12:41:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,15 +17,15 @@ terraform {
 provider "azurerm" {
   features {}
 }
-module "app1-rg" {
+#module "app1-rg" {
+ #       source = "git@github.com:subbuj1210/Practice-Devops.git"
+ #       rg_name = "app1-rg3"
+ #       rg_location = "westus"
+#}
+module "app1-vnet" {
         source = "git@github.com:subbuj1210/Practice-Devops.git"
         rg_name = "app1-rg3"
         rg_location = "westus"
-}
-module "app1-vnet" {
-        source = "git@github.com:subbuj1210/Practice-Devops.git"
-        rg_name = var.rg_name
-        rg_location = var.rg_location
         vnet_name = "app1-vnet"
         vnet_cidr = ["192.168.0.0/16"]
         web_subnet_name = "websubnet"
